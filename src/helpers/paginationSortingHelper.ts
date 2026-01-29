0713cb0597dc4d6f47458777d89227c5a2542527
mdasifalijihat <mdasifalijihat@gmail.com>
2026-01-29 13:52:39 +0600

    added fiexd get all post

--- src/helpers/paginationSortingHelper.ts
+++ src/helpers/paginationSortingHelper.ts
@@ -1,32 +1,60 @@
+import { Prisma } from "../../generated/prisma/client";
+
 type IOptions = {
-    page?: number | string;
-    limit?: number | string;
-    sortOrder?: string;
-    sortBy?: string;
-}
+  page?: number | string;
+  limit?: number | string;
+  //   sortOrder?: string;
+  sortOrder?: "asc" | "desc";
+  // sortBy?: string;
+  sortBy?: "createdAt" | "updatedAt" | "title" | "views";
+};
 
 type IOptionsResult = {
-    page: number;
-    limit: number;
-    skip: number;
-    sortBy: string;
-    sortOrder: string;
-}
+  page: number;
+  limit: number;
+  skip: number;
+  //   sortBy: string;
+  //   sortOrder: string;
+  sortBy: keyof Prisma.PostOrderByWithRelationInput;
+  sortOrder: Prisma.SortOrder;
+};
+
+// const paginationSortingHelper = (options: IOptions): IOptionsResult => {
+//   const page: number = Number(options.page) || 1;
+//   const limit: number = Number(options.limit) || 10;
+//   const skip = (page - 1) * limit;
+
+//   const sortBy: string = options.sortBy || "createdAt";
+//   const sortOrder: string = options.sortOrder || "desc";
 
 const paginationSortingHelper = (options: IOptions): IOptionsResult => {
-    const page: number = Number(options.page) || 1;
-    const limit: number = Number(options.limit) || 10;
-    const skip = (page - 1) * limit
+  const page = Number(options.page) > 0 ? Number(options.page) : 1;
+  const limit = Number(options.limit) > 0 ? Number(options.limit) : 10;
+  const skip = (page - 1) * limit;
 
-    const sortBy: string = options.sortBy || "createdAt";
-    const sortOrder: string = options.sortOrder || "desc";
-    return {
-        page,
-        limit,
-        skip,
-        sortBy,
-        sortOrder
-    }
-}
+  const allowedSortBy: (keyof Prisma.PostOrderByWithRelationInput)[] = [
+    "createAt",
+    "updatedAt",
+    "title",
+    "views",
+  ];
+
+  const sortBy = allowedSortBy.includes(
+    options.sortBy as keyof Prisma.PostOrderByWithRelationInput,
+  )
+    ? (options.sortBy as keyof Prisma.PostOrderByWithRelationInput)
+    : "createAt";
+
+  const sortOrder: Prisma.SortOrder =
+    options.sortOrder === "asc" ? "asc" : "desc";
+
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
 
 export default paginationSortingHelper;
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -2,143 +2,147 @@ import { CommentStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 
 const createComment = async (payload: {
-    content: string;
-    authorId: string;
-    postId: string;
-    parentId?: string;
+  content: string;
+  authorId: string;
+  postId: string;
+  parentId?: string;
 }) => {
-    await prisma.post.findUniqueOrThrow({
-        where: {
-            id: payload.postId
-        }
-    })
+  await prisma.post.findUniqueOrThrow({
+    where: {
+      id: payload.postId,
+    },
+  });
 
-    if (payload.parentId) {
-        await prisma.comment.findUniqueOrThrow({
-            where: {
-                id: payload.parentId
-            }
-        })
-    }
-    return await prisma.comment.create({
-        data: payload
-    })
-
+  if (payload.parentId) {
+    await prisma.comment.findUniqueOrThrow({
+      where: {
+        id: payload.parentId,
+      },
+    });
+  }
+  return await prisma.comment.create({
+    data: payload,
+  });
 };
 
 const getCommentById = async (id: string) => {
-    return await prisma.comment.findUnique({
-        where: {
-            id
+  return await prisma.comment.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      post: {
+        select: {
+          id: true,
+          title: true,
+          views: true,
         },
-        include: {
-            post: {
-                select: {
-                    id: true,
-                    title: true,
-                    views: true
-                }
-            }
-        }
-    })
-}
+      },
+    },
+  });
+};
 
 const getCommentsByAuthor = async (authorId: string) => {
-    return await prisma.comment.findMany({
-        where: {
-            authorId
+  return await prisma.comment.findMany({
+    where: {
+      authorId,
+    },
+    orderBy: { createdAt: "desc" },
+    include: {
+      post: {
+        select: {
+          id: true,
+          title: true,
         },
-        orderBy: { createdAt: "desc" },
-        include: {
-            post: {
-                select: {
-                    id: true,
-                    title: true
-                }
-            }
-        }
-    })
-}
-
+      },
+    },
+  });
+};
 
 // 1. nijar comment delete korta parbe
 // login thakte hobe
 // tar nijar comment kina ata check korta hobe
 const deleteComment = async (commentId: string, authorId: string) => {
-    const commentData = await prisma.comment.findFirst({
-        where: {
-            id: commentId,
-            authorId
-        },
-        select: {
-            id: true
-        }
-    })
+  const commentData = await prisma.comment.findFirst({
+    where: {
+      id: commentId,
+      authorId,
+    },
+    select: {
+      id: true,
+    },
+  });
 
-    if (!commentData) {
-        throw new Error("Your provided input is invalid!")
-    }
+  if (!commentData) {
+    throw new Error("Your provided input is invalid!");
+  }
 
-    return await prisma.comment.delete({
-        where: {
-            id: commentData.id
-        }
-    })
-}
+  return await prisma.comment.delete({
+    where: {
+      id: commentData.id,
+    },
+  });
+};
 
 // authorId, commentId, updatedData
-const updateComment = async (commentId: string, data: { content?: string, status?: CommentStatus }, authorId: string) => {
-    const commentData = await prisma.comment.findFirst({
-        where: {
-            id: commentId,
-            authorId
-        },
-        select: {
-            id: true
-        }
-    })
+const updateComment = async (
+  commentId: string,
+  data: { content?: string; status?: CommentStatus },
+  authorId: string,
+) => {
+  const commentData = await prisma.comment.findFirst({
+    where: {
+      id: commentId,
+      authorId,
+    },
+    select: {
+      id: true,
+    },
+  });
 
-    if (!commentData) {
-        throw new Error("Your provided input is invalid!")
-    }
+  if (!commentData) {
+    throw new Error("Your provided input is invalid!");
+  }
 
-    return await prisma.comment.update({
-        where: {
-            id: commentId,
-            authorId
-        },
-        data
-    })
-}
+  return await prisma.comment.update({
+    where: {
+      id: commentId,
+      authorId,
+    },
+    data,
+  });
+};
 
 const moderateComment = async (id: string, data: { status: CommentStatus }) => {
-    const commentData = await prisma.comment.findUniqueOrThrow({
-        where: {
-            id
-        },
-        select: {
-            id: true,
-            status: true
-        }
-    });
+  const commentData = await prisma.comment.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      status: true,
+    },
+  });
 
-    if (commentData.status === data.status) {
-        throw new Error(`Your provided status (${data.status}) is already up to date.`)
-    }
+  if (commentData.status === data.status) {
+    throw new Error(
+      `Your provided status (${data.status}) is already up to date.`,
+    );
+  }
 
-    return await prisma.comment.update({
-        where: {
-            id
-        },
-        data
-    })
-}
+  return await prisma.comment.update({
+    where: {
+      id,
+    },
+    data,
+  });
+};
 
 export const CommentService = {
-    createComment,
-    getCommentById,
-    getCommentsByAuthor,
-    deleteComment,
-    updateComment,
-    moderateComment
-}
+  createComment,
+  getCommentById,
+  getCommentsByAuthor,
+  deleteComment,
+  updateComment,
+  moderateComment,
+};
